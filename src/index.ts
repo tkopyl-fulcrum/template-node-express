daebf7237719505711a9ddc87ebdaa43250ec635
tkopyl-fulcrum <kopyl.taras@fulcrum.rocks>
2026-02-09 11:33:08 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -28,4 +28,12 @@ const main = async () => {
     });
 }
 
+app.get("/", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
 main();
